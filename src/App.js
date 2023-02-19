7f18781b488cb749599cee022e00fad2f032f9cf
dshmakov <dshmakov@voximplant.com>
2023-02-20 01:00:41 +1000

    add UI MUI theme

--- src/App.js
+++ src/App.js
@@ -6,20 +6,36 @@ import Home from "./components/pages/Home";
 import Profile from "./components/pages/Profile";
 import Interview from "./components/pages/Interview";
 import Error404 from "./components/pages/errors/404";
+import { createTheme } from '@material-ui/core/styles';
+import {ThemeProvider} from "@mui/material";
+
+// Or Create your Own theme:
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#000', // very red
+        },
+        secondary: {
+            main: '#f8f9fb', // very cyan
+        },
+    }
+});
 
 function App() {
   return (
       <BrowserRouter>
-          <SideHeader />
-          <div className="App">
-              <TopHeader />
-              <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/profile" element={<Profile />} />
-                  <Route path="/interview" element={<Interview />} />
-                  <Route path="*" element={<Error404 />} />
-              </Routes>
-          </div>
+          <ThemeProvider theme={theme}>
+              <SideHeader />
+              <div className="App">
+                  <TopHeader />
+                  <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/profile" element={<Profile />} />
+                      <Route path="/interview" element={<Interview />} />
+                      <Route path="*" element={<Error404 />} />
+                  </Routes>
+              </div>
+          </ThemeProvider>
       </BrowserRouter>
   );
 }
