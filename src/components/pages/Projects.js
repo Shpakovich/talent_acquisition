5a9ebb2de7904983c5b3508aede87ec6b7697d48
dshmakov <dshmakov@voximplant.com>
2023-02-21 18:29:11 +1000

    add grid styles for projects list

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -24,16 +24,78 @@ const ProjectsArray = [{
         username: 'User2',
         avatar: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
     }]
+},{
+    title: 'Sberseasons',
+    logo: 'https://free-png.ru/wp-content/uploads/2020/09/icon_sber-01-340x340.png',
+    tags: [
+        'Social media strategy',
+        'Social media branding'
+    ],
+    date: "23.03.2022",
+    users: [{
+        username: 'User1',
+        avatar: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80'
+    },{
+        username: 'User2',
+        avatar: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+    }]
+},{
+    title: 'Sberseasons',
+    logo: 'https://free-png.ru/wp-content/uploads/2020/09/icon_sber-01-340x340.png',
+    tags: [
+        'Social media strategy',
+        'Social media branding'
+    ],
+    date: "23.03.2022",
+    users: [{
+        username: 'User1',
+        avatar: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80'
+    },{
+        username: 'User2',
+        avatar: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+    }]
+},{
+    title: 'Sberseasons',
+    logo: 'https://free-png.ru/wp-content/uploads/2020/09/icon_sber-01-340x340.png',
+    tags: [
+        'Social media strategy',
+        'Social media branding'
+    ],
+    date: "23.03.2022",
+    users: [{
+        username: 'User1',
+        avatar: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80'
+    },{
+        username: 'User2',
+        avatar: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+    }]
+},{
+    title: 'Sberseasons',
+    logo: 'https://free-png.ru/wp-content/uploads/2020/09/icon_sber-01-340x340.png',
+    tags: [
+        'Social media strategy',
+        'Social media branding'
+    ],
+    date: "23.03.2022",
+    users: [{
+        username: 'User1',
+        avatar: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80'
+    },{
+        username: 'User2',
+        avatar: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+    }]
 }];
 
 export default class Projects extends Component {
     render() {
         return (
-            <div className="Profile">
-                {ProjectsArray.map(({title, logo, tags, date, users})=> (
-                    <RecipeReviewCard logo={logo} title={title} tags={tags} date={date} users={users} />
-                ))}
-                <Fab color="primary" aria-label="add" className="Project-Add-Button">
+            <div className="Projects">
+                <div className="Projects-grid">
+                    {ProjectsArray.map(({title, logo, tags, date, users})=> (
+                        <RecipeReviewCard className="Projects-card" logo={logo} title={title} tags={tags} date={date} users={users} />
+                    ))}
+                </div>
+                <Fab color="primary" aria-label="add" className="Projects-Add-Button">
                     <FontAwesomeIcon icon={myIcons['plus']} className='fa-2x' />
                 </Fab>
             </div>
